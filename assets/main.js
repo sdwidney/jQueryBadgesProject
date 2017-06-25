c6fb934f642effb6a7fd9cef4ef303804e30d9b5
Scott Widney <sd.widney@gmail.com>
2017-06-25 13:48:52 -0700

    Add badges

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/sdwidney.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (course of response.courses.completed) {
+        $('#badges').append('<div class="course"><h3>'+course.title+'</h3><img src="'+course.badge+'" /><a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+      }
+    }
+  });
 
 });
